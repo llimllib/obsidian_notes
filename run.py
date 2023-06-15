011a0eafb7e4b633f905e4a1e7ef8b0403d87be3
Bill Mill <bill@billmill.org>
2023-06-15 10:22:40 -0400

    give Page and Attachment dataclasses

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import argparse
 from collections import defaultdict
+from dataclasses import dataclass
 from datetime import datetime
 from html import escape
 import os
@@ -12,20 +13,81 @@ import subprocess
 import shutil
 import sys
 from time import strftime, localtime, time
-from typing import List, Optional, Tuple, Dict, Any
+from typing import List, Optional, Tuple, Dict
 
 from jinja2 import Environment, FileSystemLoader
 from markdown_it import MarkdownIt
-from mdit_py_plugins.anchors import anchors_plugin
-from mdit_py_plugins.dollarmath import dollarmath_plugin
-from mdit_py_plugins.footnote import footnote_plugin
-from mdit_py_plugins.front_matter import front_matter_plugin
+
+# https://github.com/executablebooks/mdit-py-plugins/pull/91 fixes the "not
+# exported" errors pyright reports. Remove the ignore marks once it's merged
+# and we can pull an updated version
+from mdit_py_plugins.anchors import anchors_plugin  # type: ignore
+from mdit_py_plugins.footnote import footnote_plugin  # type: ignore
+from mdit_py_plugins.front_matter import front_matter_plugin  # type: ignore
 from pygments import highlight as pygmentize
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 from strict_rfc3339 import timestamp_to_rfc3339_utcoffset
 
 JINJA = Environment(loader=FileSystemLoader("templates"))
+
+
+@dataclass
+class Attachment:
+    title: str
+    canon_title: str
+    fullpath: str
+    link_path: str
+    file: str
+    relpath: str
+    links: List[str]
+    backlinks: List[Page | Attachment]
+
+
+@dataclass
+class Page:
+    # the page's title
+    title: str
+    canon_title: str
+
+    # the file path
+    file: str
+
+    # the relative path to the markdown source
+    relpath: str
+
+    # the absolute path to the markdown source
+    fullpath: str
+
+    # titlepath is the relative path and the file name of the page stripped of
+    # its extension, for example it might be 'visualization/bar_charts'
+    titlepath: str
+    link_path: str
+
+    # links is a list of (what, exactly?)
+    links: List[str]
+    # backlinks is
+    backlinks: List[Page | Attachment]
+
+    # the mtime of the file
+    mtime: float
+
+    # file creation and modification time, ISO timestamped
+    rfc3339_ctime: str
+    rfc3339_mtime: str
+
+    # created and updated dates, formatted for humans
+    created_date: str
+    updated_date: str
+
+    # the contents of the page, in markdown
+    source: str
+
+    # the contents of the page, rendered to HTML
+    html: str = ""
+
+    # the same HTML, escaped for use in the atom feed
+    html_escaped_content: str = ""
 
 
 def info(msg: str) -> None:
@@ -53,8 +115,8 @@ MARKDOWN_RE = re.compile(r"\.md$")
 
 def outname(fname: str) -> str:
     """Turn a markdown filename into an html file name"""
-    fname = WHITESPACE_RE.sub("_", fname)
-    return MARKDOWN_RE.sub(".html", fname)
+    clean = WHITESPACE_RE.sub("_", fname)
+    return MARKDOWN_RE.sub(".html", clean)
 
 
 SANITIZE_PATH = re.compile(r"[^\w\-\._~\\\/]")
@@ -65,7 +127,7 @@ def pathname(dname: str) -> str:
     return SANITIZE_PATH.sub("_", dname)
 
 
-def mkdir(dir_: str) -> str:
+def mkdir(dir_: str | Path) -> str | Path:
     """Recursively make dir_ if it does not exist"""
     if not os.path.isdir(dir_):
         os.makedirs(dir_, exist_ok=True)
@@ -130,14 +192,13 @@ def canonical_path(relative_path: str) -> str:
     """
     path, page = os.path.split(relative_path)
     if path:
-        return f"{pathname(path)}/{canonicalize(page)}"
-    return canonicalize(page)
+        return pathname(path) + "/" + canonicalize(page)
+    return page.lower()
 
 
-# TODO: type the page object... for now I'm just calling it "Any"
 def find(
-    pages: Dict[str, Any], attachments: Dict[str, Any], link: str
-) -> Optional[Dict[str, Any]]:
+    pages: Dict[str, Page], attachments: Dict[str, Attachment], link: str
+) -> Optional[Page | Attachment]:
     """find a page referred to by `link`
 
     Pages can be linked in two ways:
@@ -149,11 +210,11 @@ def find(
     """
     clink = canonical_path(link)
     for relpath, page in pages.items():
-        if page["canon_title"] == clink or relpath == clink:
+        if page.canon_title == clink or relpath == clink:
             return page
-    for link_path, page in attachments.items():
-        if page["file"] == link or link_path == link:
-            return page
+    for link_path, attach in attachments.items():
+        if attach.file == link or link_path == link:
+            return attach
 
 
 def split_files(files: List[str]) -> Tuple[List[str], List[str]]:
@@ -165,21 +226,27 @@ def split_files(files: List[str]) -> Tuple[List[str], List[str]]:
 
 
 class FileTree:
-    def __init__(self, dir=None, page=None):
+    def __init__(
+        self, dir: Optional[str] = None, page: Optional[Page | Attachment] = None
+    ):
         self.dir = dir
-        if self.dir:
-            self.basename = os.path.basename(dir)
+        if self.dir is not None:
+            self.basename = os.path.basename(self.dir)
         self.page = page
         self.children = []
 
     def __str__(self):
-        return os.path.basename(self.dir) if self.dir else self.page["title"]
+        if self.dir:
+            return os.path.basename(self.dir)
+        elif self.page:
+            return self.page.title
+        raise AssertionError("either page or dir must be true")
 
     def __repr__(self):
         return self.__str__()
 
 
-def handle_file(path: str, root: str) -> Dict[str, Any]:
+def handle_file(path: str, root: str) -> Page | Attachment:
     """given a full path and the root of the tree, return a page dict
 
     path: full path to a file
@@ -199,50 +266,48 @@ def handle_file(path: str, root: str) -> Dict[str, Any]:
             title, _ = os.path.splitext(filename)
             titlepath = os.path.join(relpath, canonicalize(title))
             t = os.stat(path)
-            return {
+            return Page(
                 # `title` contains the title, cased as the author cased it
-                "title": title,
+                title=title,
                 # `canon_title` contains the canonicalized title
-                "canon_title": canonicalize(title),
-                "links": links,
-                "fullpath": path,
-                "link_path": os.path.join(relpath, outname(filename)),
-                "file": filename,
-                "relpath": relpath,
-                "titlepath": titlepath,
-                "source": source,
-                "backlinks": [],
-                "mtime": t.st_mtime,
+                canon_title=canonicalize(title),
+                links=links,
+                fullpath=path,
+                link_path=os.path.join(relpath, outname(filename)),
+                file=filename,
+                relpath=relpath,
+                titlepath=titlepath,
+                source=source,
+                backlinks=[],
+                mtime=t.st_mtime,
                 # would be better to put file creation time in front matter
                 # at file create time and pull it from there, but this will
                 # do for now
-                "rfc3339_ctime": rfc3339_time(t.st_ctime),
-                "rfc3339_mtime": rfc3339_time(t.st_mtime),
-                "created_date": formatted_time(t.st_ctime),
-                "updated_date": formatted_time(t.st_mtime),
-                "attachment": False,
-            }
+                rfc3339_ctime=rfc3339_time(t.st_ctime),
+                rfc3339_mtime=rfc3339_time(t.st_mtime),
+                created_date=formatted_time(t.st_ctime),
+                updated_date=formatted_time(t.st_mtime),
+            )
 
     # if it's not a markdown file, parse it as an attachment
     dir, filename = os.path.split(path)
     title, _ = os.path.splitext(filename)
     relpath = pathname(dir.removeprefix(root).lstrip("/"))
-    return {
-        "title": title,
-        "canon_title": canonicalize(title),
-        "fullpath": path,
-        "link_path": os.path.join(relpath, filename),
-        "file": filename,
-        "relpath": relpath,
-        "links": [],
-        "backlinks": [],
-        "attachment": True,
-    }
+    return Attachment(
+        title=title,
+        canon_title=canonicalize(title),
+        fullpath=path,
+        link_path=os.path.join(relpath, filename),
+        file=filename,
+        relpath=relpath,
+        links=[],
+        backlinks=[],
+    )
 
 
 def build_file_tree(
     dir: str, ignore: set[str]
-) -> Tuple[FileTree, Dict[str, Any], Dict[str, Any]]:
+) -> Tuple[FileTree, Dict[str, Page], Dict[str, Attachment]]:
     """build a file tree from a given directory
 
     dir: the directory to build the file tree from
@@ -270,12 +335,13 @@ def build_file_tree_helper(
     node: FileTree,
     ignore: set[str],
     root_path: str,
-    index: Dict[str, Any],
-    attachments: Dict[str, Any],
+    index: Dict[str, Page],
+    attachments: Dict[str, Attachment],
 ) -> FileTree:
-    # XXX: sorting the whole path is kind of janky, is it right?
+    assert node.dir
     for de in sorted(
-        os.scandir(os.path.join(root_path, node.dir)), key=lambda x: x.path.lower()
+        os.scandir(os.path.join(root_path, node.dir)),
+        key=lambda x: x.path.lower(),
     ):
         if de.name in ignore:
             continue
@@ -293,31 +359,33 @@ def build_file_tree_helper(
             # we want to index each page by its titlepath, which is something
             # like 'visualization/bar_charts'. If the page does not have a
             # titlepath attribute, assume that it's not a content page
-            if "titlepath" in page:
-                index[page["titlepath"]] = page
+            if isinstance(page, Page):
+                index[page.titlepath] = page
             else:
-                attachments[page["link_path"]] = page
+                attachments[page.link_path] = page
 
             node.children.append(FileTree(page=page))
 
     return node
 
 
-def calculate_backlinks(pages: Dict[str, Any], attachments: Dict[str, Any]) -> None:
+def calculate_backlinks(
+    pages: Dict[str, Page], attachments: Dict[str, Attachment]
+) -> None:
     for page in pages.values():
-        for link in page["links"]:
+        for link in page.links:
             linked_page = find(pages, attachments, link)
             if not linked_page:
-                print(f"unable to find link {link} in {page['title']}")
+                print(f"unable to find link {link} in {page.title}")
                 continue
-            linked_page["backlinks"].append(page)
+            linked_page.backlinks.append(page)
 
 
-def generate_lastweek_page(pages: Dict[str, Any], outdir: Path) -> None:
+def generate_lastweek_page(pages: Dict[str, Page], outdir: Path) -> None:
     today = datetime.today()
     pages_by_weeks_ago = defaultdict(list)
-    for p in reversed(sorted(pages.values(), key=lambda x: x["mtime"])):
-        daysago = (today - datetime.fromtimestamp(p["mtime"])).days
+    for p in reversed(sorted(pages.values(), key=lambda x: x.mtime)):
+        daysago = (today - datetime.fromtimestamp(p.mtime)).days
         pages_by_weeks_ago[(daysago - 1) // 7].append(p)
         if daysago > 21:
             break
@@ -328,13 +396,12 @@ def generate_lastweek_page(pages: Dict[str, Any], outdir: Path) -> None:
 
 
 def generate_index_page(
-    tree: FileTree, pages: Dict[str, Any], outdir: Path, recent: int
+    tree: FileTree, pages: Dict[str, Page], outdir: Path, recent: int
 ) -> None:
-    by_mtime = list(reversed(sorted(pages.values(), key=lambda x: x["mtime"])))
+    by_mtime = list(reversed(sorted(pages.values(), key=lambda x: x.mtime)))
     open(outdir / "index.html", "w").write(
         render(
             "index.html",
-            pages=pages,
             recently_updated=by_mtime[:recent],
             tree=tree,
         )
@@ -344,8 +411,8 @@ def generate_index_page(
     # have the rendered HTML so we can generate the atom summaries
     posts = by_mtime[:recent]
     for p in posts:
-        if "html_escaped_content" not in p:
-            p["html_escaped_content"] = escape(render_content(p))
+        if not p.html_escaped_content:
+            p.html_escaped_content = escape(render_content(p))
 
     open(outdir / "atom.xml", "w").write(
         render("atom.xml", posts=by_mtime[:recent], timestamp=rfc3339_time(time()))
@@ -375,50 +442,46 @@ MD = (
 )
 
 
-def render_content(page: Any) -> str:
+def render_content(page: Page) -> str:
     """
     Given a "page" object, render the markdown within to escaped HTML
     """
-    return MD.render(page["source"])
+    return MD.render(page.source)
 
 
-def generate_html_pages(pages: Dict[str, Any], outdir: Path) -> None:
+def generate_html_pages(pages: Dict[str, Page], outdir: Path) -> None:
     for page in pages.values():
-        output_path = outdir / page["link_path"]
+        output_path = outdir / page.link_path
 
         # Optimization: If the file has already been converted to HTML and the
         # HTML is newer than the source, don't regenerate the file
-        if (
-            os.path.isfile(output_path)
-            and page["mtime"] < os.stat(output_path).st_mtime
-        ):
+        if os.path.isfile(output_path) and page.mtime < os.stat(output_path).st_mtime:
             continue
 
         # XXX: this is a mess. make a real page object
-        page["html"] = render_content(page)
-        page["html_escaped_content"] = escape(page["html"])
+        page.html = render_content(page)
+        page.html_escaped_content = escape(page.html)
 
-        mkdir(str(outdir / page["relpath"]))
+        mkdir(str(outdir / page.relpath))
         with open(output_path, "w") as fout:
-            text = render("page.html", content=page["html"], **page)
+            text = render("page.html", page=page)
             fout.write(text)
-            page["html_content"] = text
 
 
-def copy_attachments(attachments: Dict[str, Any], outdir: Path) -> None:
+def copy_attachments(attachments: Dict[str, Attachment], outdir: Path) -> None:
     for page in attachments.values():
-        mkdir(outdir / page["relpath"])
-        shutil.copy(page["fullpath"], outdir / page["link_path"])
+        mkdir(outdir / page.relpath)
+        shutil.copy(page.fullpath, outdir / page.link_path)
 
 
-def attachment_replacer(pages: Dict[str, Any], attachments: Dict[str, Any]):
+def attachment_replacer(pages: Dict[str, Page], attachments: Dict[str, Attachment]):
     def _attachment_replacer(m: re.Match) -> str:
         filename = m.group(1)
         linked_attch = find(pages, attachments, filename)
         if not linked_attch:
             err(f"Unable to find attachment {filename}")
             return ""
-        path = linked_attch["link_path"]
+        path = linked_attch.link_path
         # assume it's an image unless it ends with PDF
         if filename.endswith(".pdf"):
             return f'<iframe src="/{path}" width="800" height="1200"></iframe>'
@@ -430,21 +493,23 @@ def attachment_replacer(pages: Dict[str, Any], attachments: Dict[str, Any]):
 IMAGE_LINK_RE = re.compile(r"!\[\[(.*?)\]\]")
 
 
-def substitute_images(pages: Dict[str, Any], attachments: Dict[str, Any]) -> None:
+def substitute_images(
+    pages: Dict[str, Page], attachments: Dict[str, Attachment]
+) -> None:
     replacer = attachment_replacer(pages, attachments)
     for page in pages.values():
-        page["source"] = IMAGE_LINK_RE.sub(replacer, page["source"])
+        page.source = IMAGE_LINK_RE.sub(replacer, page.source)
 
 
 # maybe move to https://github.com/jsepia/markdown-it-wikilinks eventually?
-def crosslink_replacer(pages: Dict[str, Any]):
+def crosslink_replacer(pages: Dict[str, Page]):
     def _crosslink_replacer(m: re.Match) -> str:
         title = m.group(1)
         linked_page = find(pages, {}, title)
         if not linked_page:
             err(f"Unable to find page {title}")
             return ""
-        return f'<a href="/{linked_page["link_path"]}">{title}</a>'
+        return f'<a href="/{linked_page.link_path}">{title}</a>'
 
     return _crosslink_replacer
 
@@ -456,10 +521,10 @@ def crosslink_replacer(pages: Dict[str, Any]):
 CROSSLINK_RE = re.compile(r"\[\[(.*?)(?:\|.*?)?\]\]")
 
 
-def substitute_crosslinks(pages: Dict[str, Any]) -> None:
+def substitute_crosslinks(pages: Dict[str, Page]) -> None:
     replacer = crosslink_replacer(pages)
     for page in pages.values():
-        page["source"] = CROSSLINK_RE.sub(replacer, page["source"])
+        page.source = CROSSLINK_RE.sub(replacer, page.source)
 
 
 def parse(mddir: str, recent: int, ignore: Optional[set[str]] = None):
@@ -472,9 +537,9 @@ def parse(mddir: str, recent: int, ignore: Optional[set[str]] = None):
     """
     # make the type checker happy
     ignore = ignore if ignore else set()
-    mddir = os.path.normpath(os.path.expanduser(mddir))
+    dir = os.path.normpath(os.path.expanduser(mddir))
 
-    tree, pages, attachments = build_file_tree(mddir, ignore)
+    tree, pages, attachments = build_file_tree(dir, ignore)
     calculate_backlinks(pages, attachments)
 
     outdir = Path(mkdir("./output"))
