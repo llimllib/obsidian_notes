e9495535cf21815fe223be4de5be65280f95408c
Bill Mill <bill@billmill.org>
2023-10-30 08:51:33 -0400

    feature: make git stat work properly
    and move it into its own function with better commenting

--- run.py
+++ run.py
@@ -273,6 +273,35 @@ class FileTree:
         return self.__str__()
 
 
+def gitstat(dir: str, path: str) -> GitStat:
+    """return the created and modified times for a file from git"""
+    # Here's an example of what the output looks like for this:
+    #
+    # $ git -C . log --pretty="format:%cI %aI" README.md
+    # 2023-10-30T08:18:52-04:00 2023-10-30T08:18:52-04:00
+    # 2022-04-28T21:54:00-04:00 2022-04-28T21:54:00-04:00
+    # 2022-04-09T22:09:23-04:00 2022-04-09T22:09:23-04:00
+    # 2022-04-08T21:36:48-04:00 2022-04-08T21:36:48-04:00
+    #
+    # we need to use the -C argument to tell git to look in the notes
+    # repository instead of this repository
+    #
+    # possibly I should add --follow so that this persists through renames?
+    # though maybe I ought to consider a rename a recreation of a file? not
+    # clear to me whether it's worth it or not.
+    times = subprocess.check_output(
+        ["git", "-C", dir, "log", "--pretty=format:%aI %cI", path]
+    ).decode("utf8")
+
+    # The modified time is the second timestamp on the first line
+    mtime = rfc3339_to_timestamp(times[0].split(" ")[1])
+
+    # The created time is the first timestamp on the last line
+    ctime = rfc3339_to_timestamp(times[-1].split(" ")[0])
+
+    return GitStat(ctime, mtime)
+
+
 def handle_file(path: str, root: str, use_git_times: bool) -> Page | Attachment:
     """given a full path and the root of the tree, return a page dict
 
@@ -296,15 +325,7 @@ def handle_file(path: str, root: str, use_git_times: bool) -> Page | Attachment:
             # if use_git_times is true, assume that the file is stored in git,
             # and get ctime and mtime from git.
             if use_git_times:
-                ctime, mtime = map(
-                    rfc3339_to_timestamp,
-                    subprocess.check_output(
-                        ["git", "-C", dir, "log", "-1", "--pretty=format:%aI %cI", path]
-                    )
-                    .decode("utf8")
-                    .split(" "),
-                )
-                t = GitStat(ctime, mtime)
+                t = gitstat(dir, path)
             else:
                 t = os.stat(path)
 
